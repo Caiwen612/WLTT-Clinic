153d2a9448c921de5e344577e323b221d55baffd
BananaKing123 <78149270+BananaKing123@users.noreply.github.com>
2022-08-06 18:03:24 +0800

    First push project

--- src/client/Main.java
+++ src/client/Main.java
@@ -2,6 +2,7 @@ package client;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world hi");
+        System.out.println("hi");
     }
 }
