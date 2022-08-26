07146ca8bbb9e4c19fcc86605ffc623cf1e33dcf
wilsonyau02 <yauwilson02@gmail.com>
2022-08-26 11:05:04 +0800

    Add Medicine and Pharmacist

---
+++ src/entity/Medicine.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Medicine {
+    private String Id;
+    private String name;
+    private String function;
+    private int quantity;
+
+
+}
---
+++ src/entity/Pharmacist.java
@@ -0,0 +1,29 @@
+package entity;
+
+public class Pharmacist {
+    private String ID;
+    private String name;
+
+    public Pharmacist(){}
+
+    public Pharmacist(String ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
