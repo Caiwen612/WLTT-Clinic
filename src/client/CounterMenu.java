7a4e003b6b74410e6e4f316d76d657920d3847c4
LanKeEn-TARC <79739372+LanKeEn-TARC@users.noreply.github.com>
2022-08-27 17:06:25 +0800

    patient entity

---
+++ src/client/CounterMenu.java
@@ -0,0 +1,14 @@
+package client;
+
+public class CounterMenu {
+    public static void menu(){
+        System.out.println("Counter: ");
+        System.out.println("[1] Search for Patient");
+        System.out.println("[2] Add New Patient");
+        System.out.println("[3] Queue Number");
+    }
+
+
+
+
+}
--- src/entity/Patient.java
+++ src/entity/Patient.java
@@ -1,5 +1,68 @@
 package entity;
 
 public class Patient {
+    private String patientName;
+    private String icNo;
+    private String phoneNo;
+    private String address;
+    private String DOB;
 
+    /*public Patient(String name, String icNo, String phoneNo, String address, String DOB ){
+        this.patientName = name;
+        this.icNo = icNo;
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.DOB = DOB;
+    }*/
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getIcNo() {
+        return icNo;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setIcNo(String icNo) {
+        this.icNo = icNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientName='" + patientName + '\'' +
+                ", icNo='" + icNo + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", address='" + address + '\'' +
+                ", DOB='" + DOB + '\'' +
+                '}';
+    }
 }
